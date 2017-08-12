3da9a7f2265c26fc0cb7e5a2ee225d09df313e58
cycoflex <cycoflex@gmail.com>
2017-08-12 14:08:03 +0200

    section accueil 1 et 2, css rajout des section dans la constuction du html

--- expedition/templates/section-accueil_2.php
+++ expedition/templates/section-accueil_2.php
@@ -3,6 +3,8 @@
 		<figure>
 			<img src="<?php echo $urlRoot; ?>/assets/img/accueil/image_accueil_1.png">
 		</figure>
+		<span>01</span>
+		<div></div>
 		<article>
 			<h2>Qui sommes-nous ?</h2>
 			<p>Haec et huius modi quaedam innumerabilia ultrix facinorum impiorum bonorumque praemiatrix aliquotiens operatur Adrastia atque utinam semper quam vocabulo duplici etiam Nemesim appellamus: ius quoddam sublime numinis efficacis, humanarum mentium opinione lunari circulo superpositum, vel ut definiunt alii, substantialis tutela generali potentia partilibus praesidens fatis, quam theologi veteres fingentes Iustitiae filiam ex abdita quadam aeternitate tradunt omnia despectare terrena.
